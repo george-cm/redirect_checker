32bcbd12482d9b67af12f7123d9b885b34e63c40
george-cm <george.murga+github@gmail.com>
2020-02-28 13:18:51 +0200

    Fix bug

--- redirect_check.py
+++ redirect_check.py
@@ -7,16 +7,25 @@ import argparse
 import logging
 import os
 from datetime import datetime
+import chardet
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 MAX_REDIRECTS = 10
 
+def detect_encoding(fname):
+    import chardet
+    rawdata = open(fname, 'rb').read()
+    result = chardet.detect(rawdata)
+    charenc = result['encoding']
+    return charenc
+
 def check_redirect(req_session, source_url, target_url, logger, auth=None, retries=30):
   result = []
   redirect_chain = []
-  if target_url[-1] == '/':
-    target_url = target_url[:-1]
+  target_url = target_url.rstrip('/')
+  # if target_url[-1] == '/':
+  #   target_url = target_url[:-1]
   # processing the query string in order to reattach it to the target_url
   # source_parsed = urlparse.urlparse(source_url)
   # source_query_string = source_parsed.query
@@ -32,16 +41,28 @@ def check_redirect(req_session, source_url, target_url, logger, auth=None, retri
       req = req_session.get(source_url, allow_redirects=True, verify=False, auth=auth)
       if len(req.history) != 0:
         for res in req.history:
-          Location = res.headers['Location']
-          # print(Location)
-          redirect_chain.append(Location)
-          if Location == target_url:
+          Location = res.headers['Location'].rstrip('/')
+          redirect_chain.append(f"{res.status_code}::{Location}")
+        if len(req.history) == 1:
+          if req.history[-1].headers['Location'].rstrip('/') == target_url:
             result.append(res.status_code)
             result.append('yes')
+          else:
+            result.append(res.status_code)
+            result.append('no')
+        else:
+          if req.history[-1].headers['Location'].rstrip('/') == target_url:
+            result.append('multiple')
+            result.append('yes')
+          else:
+            result.append('multiple')
+            result.append('no')
+
+          # print(Location)
             # print('Redirect matches the list.')
             # print(f'Redirect status code: {res.status_code}')
-        if not result:
-          result = ['n/a', 'no']
+        # if not result:
+        #   result = ['n/a', 'no', []]
         result.append(redirect_chain)
       else:
         result = [req.status_code, 'no', [f'No redirects found']]
@@ -107,22 +128,33 @@ epilog="""""", formatter_class=argparse.RawTextHelpFormatter)
 
   output_file = file_name[:-4] + '_' + '{:%Y%m%d%H%M%S}'.format(datetime.now()) + '.csv'
   # for csv_file in csv_files:
-  with open(file_name, encoding='utf-8-sig') as input_csvfile:
+  
+  enc = detect_encoding(file_name)
+  with open(file_name, encoding=enc) as input_csvfile:
+    total_lines = sum(1 for line in input_csvfile)
+    if not args.no_header:
+      total_lines -=1
+
+  with open(file_name, encoding=enc) as input_csvfile:
     reader = csv.reader(input_csvfile, delimiter=',', quotechar='"')
-    with open(output_file, 'w', encoding='utf-8-sig', newline='') as output_csvfile:
+    with open(output_file, 'w', encoding=enc, newline='') as output_csvfile:
       writer = csv.writer(output_csvfile, delimiter=',', 
                           quotechar='"', quoting=csv.QUOTE_MINIMAL)
       writer.writerow(['Source Url', 'Target Url', 'Redirect Code', 
                       'Redirect Ok', 'Redirect Chain'])
+      
+      if not args.no_header:
+        header = next(reader)
       for idx, row in enumerate(reader):
-        if not args.no_header:
-          if idx == 0:
-            continue
+        # if not args.no_header:
+        #   if idx == 0:
+        #     continue
         # print(f'Checking: {row[0]}')
         result = check_redirect(s, row[0], row[1], logger, auth, 200)
         # print(f'status_code: {result[0]}, matches: {result[1]}, chain: {"|".join(result[2])}')
-        logger.info(f'status_code: {result[0]}, matches: {result[1]}, source_url: "{row[0]}", chain: {"|".join(result[2])}')
+        logger.info(f'{idx+1}/{total_lines} status_code: {result[0]}, matches: {result[1]}, source_url: "{row[0]}", chain: {"|".join(result[2])}')
         writer.writerow([row[0], row[1], result[0], result[1], '|'.join(result[2])])
+  # print(enc)
 
 
 if __name__ == '__main__':
